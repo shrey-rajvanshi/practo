7d4259282e14c970ddd950877c13ad730709a21d
shrey rajvanshi <shreykantrajvanshi@gmail.com>
2015-06-17 01:54:56 +0530

    Implemented Doctor-Speciality Association

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String,Text,Table,ForeignKey
-
+from sqlalchemy.orm import relationship,backref
 from database import Base
 
 class Doctor(Base):
@@ -12,6 +12,7 @@ class Doctor(Base):
    recommendations = Column(Integer)
    qualification = Column(Text)
    city=Column(String(20))
+   specialities = relationship("Speciality",secondary="association")
 
 
    def __init__(self, name=None, locality=None,city=None,
@@ -29,4 +30,31 @@ class Doctor(Base):
 
    def __repr__(self):
         return '<Doctor %r>' % (self.name)
+
+
+class Speciality(Base):
+    __tablename__ = 'Speciality_details'
+    id = Column(Integer,primary_key = True)
+    name = Column(String(40),unique=True)
+    doctors = relationship(
+        "Doctor",
+        secondary='association'
+    )
+
+    def __init__(self):
+        self.name = name
+
+    def __repr__(self):
+        return '<Speciality %r>' % (self.name)
+
+
+
+class doc_spec(Base):
+    __tablename__ = 'association'
+    doc_id = Column(Integer, ForeignKey('doctor_details.id'), primary_key=True)
+    spec_id = Column(Integer, ForeignKey('Speciality_details.id'), primary_key=True)
+    extra_data = Column(String(50))
+    speciality_rel = relationship(Speciality, backref=backref("associated_speciality"))
+    doctor_rel = relationship(Doctor, backref=backref("associated_doctor"))
+
 
--- app/views.py
+++ app/views.py
@@ -6,9 +6,35 @@ from app import app
 @app.route('/', methods=['GET', 'POST'])
 def home():
     doctors = Doctor.query.all()
-
+    specs = Speciality.query.all()
     return render_template('index.html', doctors=doctors)
 
+@app.route('/search/<location>')
+def searchbylocation(location):
+    doctors = Doctor.query.filter(Doctor.city==location)
+    return render_template('index.html', doctors=doctors)
+
+
+@app.route('/searchspec/<speciality>', methods=['GET', 'POST'])
+def searchbyspeciality(speciality):
+    listOfDocIds = doc_spec.query.filter(doc_spec.doc_id==1)
+    users=[]
+    for i in listOfDocIds:
+        doctors.append(Doctor.query.get(int(i.spec_id)))
+    return render_template('index.html', doctors=doctors)
+
+@app.route('/search/<location>/<speciality>', methods=['GET', 'POST'])
+def searchbyspecialitynlocation(location,speciality):
+    searchedSpecId=Speciality.query.filter(Speciality.name==speciality)[0].id
+    listOfDoc_specs = doc_spec.query.filter(doc_spec.spec_id==searchedSpecId)
+    doctors=[]
+    stra=""
+    for doc_spec_object in listOfDoc_specs:
+        stra+=str(doc_spec_object)
+        doc_id=doc_spec_object.doc_id
+        if(Doctor.query.get(int(doc_id)).city ==location):
+            doctors.append(Doctor.query.get(int(doc_id)))
+    return render_template('index.html', doctors=doctors)
 
 @app.route('/home')
 def landingpage():
