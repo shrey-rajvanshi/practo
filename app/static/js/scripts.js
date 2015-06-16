9985689efd7139f6e4dca8f69a6c51d7f265b1ab
shrey rajvanshi <shreykantrajvanshi@gmail.com>
2015-06-17 04:58:23 +0530

    home page CSS JS HTML

---
+++ app/static/js/scripts.js
@@ -0,0 +1,7 @@
+function search(){
+	city =document.getElementById("location").value;
+	speciality= document.getElementById("specialization").value;
+	if(speciality="")	{window.location+=("search/"+city);}
+	else{
+	window.location+=("search/"+city+"/"+speciality);}
+}
