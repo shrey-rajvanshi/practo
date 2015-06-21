26eb444ec1410ac120a672944f4f887749c13710
shrey rajvanshi <shreykantrajvanshi@gmail.com>
2015-06-21 17:35:11 +0530

    Fixed minor mistake

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -2,7 +2,7 @@
 function search(){
 	city =document.getElementById("location").value;
 	speciality= document.getElementById("specialization").value;
-	if(speciality="")	{window.location+=("search/"+city);}
+	if(speciality=="")	{window.location+=("search/"+city);}
 	else{
 	window.location+=("search/"+city+"/"+speciality);}
 }
